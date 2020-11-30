3ba296d195df7fc4c30d7bc465f7c8c793b62ba7
Berend Weij <berend@studiozoetekauw.nl>
2020-11-30 09:55:59 +0100

    adds first setup

---
+++ app.js
@@ -0,0 +1,4 @@
+// Use this function to setup 3D models, Images etc you need at the start of your application.
+function Setup() {
+  console.log(`Starting setup!`);
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# `Ma WebXR`
+
+#### Een WebXR library voor 1e jaars SD
+Binnen deze repository 
